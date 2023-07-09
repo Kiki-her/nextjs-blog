9bb6221143c3d25fcebc20741062bd5849c0b012
kiki <k.kazaneko@gmail.com>
2023-07-09 19:11:00 +0900

    formatted date and content

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,6 +1,8 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
+import Date from "../../components/date";
+import utilStyle from "../../styles/utils.module.css";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -25,12 +27,13 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyle.headingXL}>{postData.title}</h1>
+        <div className={utilStyle.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      </article>
     </Layout>
   );
 }
